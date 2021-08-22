66da30ff376f6881553e07f26e1ed67b290a5715
NikhilIgnattius <89331788+NikhilIgnattius@users.noreply.github.com>
2021-08-22 12:21:03 +0530

    Add files via upload

---
+++ Increment Decrement opretators.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  increment decrement opretators
+//
+//  Created by Kanix on 05/08/21.
+//
+
+#include <stdio.h>
+
+int main() {
+    int a=31,b=23;
+    printf("\nBefore Incrementing:a=%d",a);
+    a++;
+    printf("\nAfter Incrementing :a=%d",a);
+    
+    printf("\nBefore Decrementing:b=%d",b);
+    b--;
+    printf("\nAfter Decrementing :b=%d",b);
+    
+    getchar();
+    
+}
