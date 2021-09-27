fa7899a14c80b7f62fdb2573ea35963522f63b59
Nikhil Ignatius <89331788+NikhilIgnattius@users.noreply.github.com>
2021-09-27 11:55:20 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,112 @@
+//
+//  main.c
+//  Bass or acoustic
+//
+//  Created by Nikhil Ignatius V on 27/09/21.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int response,response1,response2;
+    
+    printf("1.Acoustic");
+    printf("\n2.Bass");
+    printf("\nWhich guitar string name you want to know ?:");
+    scanf("%d",&response);
+    switch (response) {
+        case 1:
+            printf("Enter the guitar string number from bottom to top");
+            printf("\n\t1.String no:1");
+            printf("\n\t2.String no:2");
+            printf("\n\t3.String no:3");
+            printf("\n\t4.String no:4");
+            printf("\n\t5.String no:5");
+            printf("\n\t6.String no:6");
+            printf("\nType the string number to know the name ?:");
+            scanf("%d",&response1);
+            
+            switch (response1) {
+                case 1:
+                    printf("\nThis is the higher E string which is string no:1\n\n");
+                    exit(0);
+                    break;
+                    
+                case 2:
+                    printf("\nThis is the B string which is string no:2\n\n");
+                    exit(0);
+                    break;
+                    
+                case 3:
+                    printf("\nThis is the G string which is string no:3\n\n");
+                    exit(0);
+                    break;
+                    
+                case 4:
+                    printf("\nThis is the D string which is string no:4\n\n");
+                    exit(0);
+                    break;
+                    
+                case 5:
+                    printf("\nThis is the A string which is string no:5\n\n");
+                    exit(0);
+                    break;
+                    
+                case 6:
+                    printf("\nThis is the lower E string which is string no:6\n\n");
+                    exit(0);
+                    break;
+                    
+                default:
+                    printf("\nPlease select any of the above given number\n\n");
+                    exit(0);
+                    break;
+                    
+                    
+        }
+        case 2:
+            printf("Enter the guitar string number from bottom to top");
+            printf("\n\t1.String no:1");
+            printf("\n\t2.String no:2");
+            printf("\n\t3.String no:3");
+            printf("\n\t4.String no:4");
+            printf("\nType the string number to know the name ?:");
+            scanf("%d",&response2);
+            switch (response2) {
+              
+                    
+        case 1:
+            printf("\nThis is the G string which is string no:1\n\n");
+            exit(0);
+            break;
+            
+        case 2:
+            printf("\nThis is the D string which is string no:2\n\n");
+            exit(0);
+            break;
+            
+        case 3:
+            printf("\nThis is the A string which is string no:3\n\n");
+            exit(0);
+            break;
+            
+        case 4:
+            printf("\nThis is the E string which is string no:4\n\n");
+            exit(0);
+            break;
+                    
+        default:
+            printf("\nPlease select any of the above given number\n\n");
+            exit(0);
+            break;
+        }
+        default:
+            printf("\nPlease select any of the above given number\n\n");
+        
+    }
+        
+        
+    }
+
